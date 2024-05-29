ae43082818bfdf356785964aec2b19162901d504
Umbert <umbo@unumed.com>
2024-05-29 09:19:05 +0200

    Feat: Enable CIS 3.0.0 when cis option is selected

--- modules/securityhub-baseline/main.tf
+++ modules/securityhub-baseline/main.tf
@@ -43,6 +43,14 @@ resource "aws_securityhub_invite_accepter" "invitee" {
 resource "aws_securityhub_standards_subscription" "cis" {
   count = var.enable_cis_standard ? 1 : 0
 
+  standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/cis-aws-foundations-benchmark/v/3.0.0"
+
+  depends_on = [aws_securityhub_account.main]
+}
+
+resource "aws_securityhub_standards_subscription" "cis" {
+  count = var.enable_cis_standard ? 1 : 0
+
   standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/cis-aws-foundations-benchmark/v/1.4.0"
 
   depends_on = [aws_securityhub_account.main]
